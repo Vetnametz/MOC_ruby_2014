e08af3a7d211bb1de6c4a8d3c69754a86c14f31b
Vetnametz <vitaltrachenko@gmail.com>
2014-12-16 11:25:33 +0200

    initial commit

---
+++ home_work_4/my_script.rb
@@ -0,0 +1,57 @@
+# Три крапки означають що ваш скрипт має прийняти змінену строку і обробити її якомога краще,
+# тобто на вході ви отримуєте дані, а вам треба окрім того що створити відповідну структуру об'єктів,
+# ще й написати додаткові методи, які полегшують життя при роботі із цими об'єктами.
+#
+#     Приклади методів:
+#
+# adult? - відповідає так чи ні з огляду на вік
+# twitter_account? - відповідно залежно від наявності посилання на профайл твіттера
+# have_hobbies? - відповідно до того чи є в профайлі хоббі
+# Ну і інші ознаки які ви можете придумати для людини також опишіть.
+# Посилання на результат постіть тут, мені так легше перевіряти.Дякую.
+
+require 'json'
+
+RESPONSE='{"person":{
+"personal_data":{"name": "John Smith", "gender":"male", "age":22},
+"social_profiles":["http://facebook.com/john","http://twitter.com/john","http://smith.ru"],
+"additional_info":{"hobby":["pubsurfing","drinking","hiking"],
+"pets":[{"name":"Mittens","species":"Felis silvestris catus"}]}
+}}'
+
+# RESPONSE='{"person":{
+# "personal_data":{"name": "John Smith", "gender":"male", "age":56 ......},
+# "social_profiles":["http://facebook....","http://twitter...","http://", .... ],
+# "additional_info":{"hobby":["pubsurfing","drinking","hiking"...],
+# "pets":[{"name":"Mittens","species":"Felis silvestris catus"},],...
+# }}'
+# response = RESPONSE.gsub(/(,\.)|(, \.)|\./,'')
+
+response = JSON.parse(RESPONSE)
+
+module PersonInformation
+
+  def adult(age)
+    (age > 21) ? true : false
+  end
+
+  def has_twitter(social)
+    social.each do |elem|
+      isTrue = elem == "http://twitter.com/john"
+      if isTrue
+        return true
+      end
+    end
+  end
+
+end
+
+if response.key?('person')
+  person_object = Struct.new("Person", *response["person"].keys.collect(&:to_sym))
+  person = person_object.new(*response["person"].values)
+  person.extend(PersonInformation)
+  age = person.personal_data['age']
+  p person.adult(age)
+  social = person.social_profiles
+  p person.has_twitter(social)
+end
